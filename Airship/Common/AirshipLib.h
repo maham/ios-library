37274cf738c7360305cc0e009c024b5e11908816
Martin Hammarbrink <martin@hammarbrink.com>
2014-04-02 11:14:59 +0200

    Created a unified header and added minimum needed headers to it. More header might need to be added later.

---
+++ Airship/Common/AirshipLib.h
@@ -0,0 +1,12 @@
+//
+//  AirshipLib.h
+//  AirshipLib
+//
+//  Created by Mbg114 on 2014-04-02.
+//
+//
+#import <Foundation/Foundation.h>
+
+#import <AirshipLib/UAirship.h>
+#import <AirshipLib/UAConfig.h>
+#import <AirshipLib/UAPush.h>
